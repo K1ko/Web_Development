c428cf04ebdc1c1b95f88399fe327828451d5206
Kristián Červenka <kristian.cervenka@gmail.com>
2023-01-11 13:31:20 +0100

    :sparkles: calculator in Ruby

---
+++ calculator.rb
@@ -0,0 +1,20 @@
+# Get the user input
+puts "Enter the first number:"
+num1 = gets.to_f
+
+puts "Enter the second number:"
+num2 = gets.to_f
+
+puts "Enter the operation (+, -, *, /):"
+operation = gets.chomp
+
+# Perform the calculation
+result = case operation
+when "+" then num1 + num2
+when "-" then num1 - num2
+when "*" then num1 * num2
+when "/" then num1 / num2
+end
+
+# Print the result
+puts "The result is: #{result}"
